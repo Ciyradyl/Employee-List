b3a9df96e735f236495c005a77034b78d84a7248
hilmi <kaangenc98@gmail.com>
2022-12-19 16:22:34 +0300

    Updating CSS with respect to selected item. (color correction, typo fix)

--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -1,13 +1,20 @@
-
+import React from "react";
+import { Nav } from "react-bootstrap";
 
 const NaviBar = () => {
+  return (
+    <Nav>
+      <Nav.Item>
+        <Nav.Link href="/">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/">About</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/">Contact</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+};
 
-
-    return(
-        <div className="nav">
-            <h1 className="header-title">Employee List</h1>
-        </div>
-    )
-}
-
-export default NaviBar
+export default NaviBar;
